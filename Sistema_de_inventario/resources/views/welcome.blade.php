0e42fb0c3fea92de8210d340697aeccacc04b3ee
Josue Zelaya <josuen3ds@gmail.com>
2024-04-29 18:57:57 -0600

    Se crea una pequeña demostracion de como usar html y php en laravel

--- Sistema_de_inventario/resources/views/welcome.blade.php
+++ Sistema_de_inventario/resources/views/welcome.blade.php
@@ -7,8 +7,12 @@
     <!--Este codigo sirve para indicar que estilos usara Tailwind y en donde se ubica-->
     @vite('resources/css/app.css')
 </head>
+<!--Pequeña demostracion de como se puede utilizar php y html juntos,
+si no se coloca el '@php' y su finalizacion '@endphp', el codigo php no se ejecutara, solo se mostrara en la pagina -->
+@php $nombre = 'Pepe'; @endphp
 <body>
-    <h1 class="text-3xl font-bold">Hola mundo!!!!</h1>    
+    
+    <h1 class="text-3xl font-bold">Hola {{$nombre}}</h1>    
 
 </body>
 </html>
